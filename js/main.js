6ca0fbff8fcc3d3cfa3d51658fddac5ff58aefdb
роман немерцев <nemercevroman@gmail.com>
2025-08-31 23:57:31 +0300

    create and config mobile burger-menu

--- js/main.js
+++ js/main.js
@@ -42,3 +42,14 @@ var swiper = new Swiper(".page__slider-wrapper", {
 
 // accordion
 new Accordion(".accordion-container");
+
+// burger menu
+const header = document.querySelector(".page__header");
+const headerMobile = document.querySelector(".page__header-mobile");
+const headerBurger = document.querySelector(".page__header-burger");
+const headerWrapper = document.querySelector(".page__header-wrapper");
+headerBurger.addEventListener("click", () => {
+  headerMobile.classList.toggle("active");
+  headerBurger.classList.toggle("active");
+  headerWrapper.classList.toggle("active");
+});
